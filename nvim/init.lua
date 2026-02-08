6b51da86f341b8a478f70017cfcc36dd24073a9c
Gabriel Barbosa Santos <gabrielsantos.gbs@outlook.com>
2026-02-08 19:02:58 -0300

    fix printing bind and add spell checker for .txt files

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- turn on spellchecker only for .txt files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "text",
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = { "pt_br", "en" }
+  end,
+})
